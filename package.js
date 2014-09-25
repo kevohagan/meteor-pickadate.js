40902401a694d3a7b0468485d8b1e6649264bcf9
Kevin O'Hagan <kevinohagan@gmail.com>
2014-09-25 10:24:32 +0200

    quick update to 0.9, should check if naming is correct

--- package.js
+++ package.js
@@ -1,9 +1,11 @@
 Package.describe({
-  summary: "A Meteor.js wrapper for Pickadate.js (http://amsul.ca/pickadate.js/) The mobile-friendly, responsive, and lightweight jQuery date & time input picker."
+  summary: "A Meteor.js wrapper for Pickadate.js (http://amsul.ca/pickadate.js/) The mobile-friendly, responsive, and lightweight jQuery date & time input picker.",
+  name: "nickw:pickadate",
+  version: "3.5.3"
 });
 
 Package.on_use(function (api, where) {
-  api.use('jquery', 'client');
+  api.use(['jquery','less'], 'client');
   api.add_files([
     'js/picker.js',
     'js/picker.date.js',
